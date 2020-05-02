d5826c446fb2f80c9cfe03fccbc32b4664267b93
deveshsjain <42722660+deveshsjain@users.noreply.github.com>
2020-05-01 20:01:57 -0400

    RandomForest

---
+++ RandomForest.R
@@ -0,0 +1,31 @@
+#cleaning environment
+rm(list = ls())
+
+#load csv
+data_var<-read.csv('C:/Users/deves/Desktop/Class/CS 513 KDD/Final Project/attrition_data.csv')
+
+#Replace NA values with 0000
+data_var$TERMINATION_YEAR[is.na(data_var$TERMINATION_YEAR)]<-0000
+
+#Omiting Employee ID and columns with MeanGini less than 5.0
+
+#Convert JOB_GROUP to numeric as randomForest cannot handle categorical predictors with more than 53 categories.
+df1<-data_var[-c(1,6,13,15,16,18,19)]
+df1$JOB_GROUP<-as.numeric(df1$JOB_GROUP)
+
+#splitting data into test and training
+sample_data<-sample(nrow(df1),0.70*nrow(df1))
+training_data<-df1[sample_data,]
+test_data<-df1[-sample_data,]
+
+#Creating fit model with target variable against the rest
+library(randomForest)
+rffit <- randomForest(STATUS ~., data=training_data, ntree=30, keep.forest=TRUE, importance=TRUE)
+
+#plotting fit to check optimal number of trees for best accuracy
+plot(rffit)
+
+pred<-predict(rffit,test_data)
+table(test_data$STATUS, pred)
+
+
