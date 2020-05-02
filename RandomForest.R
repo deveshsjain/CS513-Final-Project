f7b2b510e385d1b0ab72b29718d8688df9a94472
deveshsjain <42722660+deveshsjain@users.noreply.github.com>
2020-05-01 22:20:28 -0400

    RandomForest

--- RandomForest.R
+++ RandomForest.R
@@ -2,15 +2,15 @@
 rm(list = ls())
 
 #load csv
-data_var<-read.csv('YOUR_PATH_TO_CSV')
+data_var<-read.csv('PATH_TO_CSV')
 
 #Replace NA values with 0000
 data_var$TERMINATION_YEAR[is.na(data_var$TERMINATION_YEAR)]<-0000
 
 #Omiting Employee ID and columns with MeanGini less than 5.0
+df1<-data_var[-c(1,6,13,15,16,18,19)]
 
 #Convert JOB_GROUP to numeric as randomForest cannot handle categorical predictors with more than 53 categories.
-df1<-data_var[-c(1,6,13,15,16,18,19)]
 df1$JOB_GROUP<-as.numeric(df1$JOB_GROUP)
 
 #splitting data into test and training
@@ -23,9 +23,10 @@ library(randomForest)
 rffit <- randomForest(STATUS ~., data=training_data, ntree=30, keep.forest=TRUE, importance=TRUE)
 
 #plotting fit to check optimal number of trees for best accuracy
-plot(rffit)
+plot(rffit, main = "Fit Plot")
 
+#Predict test data results
 pred<-predict(rffit,test_data)
+
+# Making the Confusion Matrix 
 table(test_data$STATUS, pred)
-
-
