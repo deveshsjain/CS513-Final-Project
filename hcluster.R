3997c28e2f6b0954455188649c8717fa165874c7
amandag0mes <32399201+amandag0mes@users.noreply.github.com>
2020-05-06 18:04:19 -0400

    updated hcluster

--- hcluster.R
+++ hcluster.R
@@ -1,23 +1,21 @@
-
 rm(list=ls())
-
 # load in data
-attrition <- read.csv("C:/Users/agome/Documents/attrition_data.csv",na.strings = " ")
+attrition <- read.csv("C:/Users/agome/Documents/attrition_data.csv")
 # set NA values in Termination Year to 0
 attrition$TERMINATION_YEAR[is.na(attrition$TERMINATION_YEAR)]<-0000
 #converting all columns to factor type
-attrition[] <- lapply(attrition, function(x) as.factor(x))
-
+attrition[] <- lapply(attrition, function(x) as.numeric(x))
 # remove unnecessary columns from data frame
 df1<-attrition[-c(1,6,13,15,16,18,19)]
-
+# sample_data<-sample(nrow(df1),0.70*nrow(df1))
+# training<-df1[sample_data,]
+# test<-df1[-sample_data,]
 # calculating distribution
-attrDist<-dist(df1[,14])
-
+attrDist<-dist(df1[,-14])
 #calculating and plotting hcluster
 results<-hclust(attrDist)
 plot(results)
-
-hclust_3<-cutree(results,3)
-table(hclust_3, df1[,14])
-
+hclust_2<-cutree(results,2)
+#is.vector(hclust_2)
+table(hclust_2, df1[,14])
+#prediction <- predict(hclust_2, test[,20])
