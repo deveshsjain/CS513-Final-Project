91ce1e783fa51cd802105ddb7c6e1a0618c32ae7
amandag0mes <32399201+amandag0mes@users.noreply.github.com>
2020-05-06 21:51:47 -0400

    Update CART.R

--- CART.R
+++ CART.R
@@ -4,7 +4,7 @@ library(rpart.plot)  			# Enhanced tree plots
 library(rattle)           # Fancy tree plot
 library(RColorBrewer) 
 # load in data
-attrition <- read.csv("C:/Users/agome/Documents/attrition_data.csv", header =TRUE)
+attrition <- read.csv('PATH_TO_CSV', header = TRUE)
 
 # set NA values in termination year to 0
 attrition$TERMINATION_YEAR[is.na(attrition$TERMINATION_YEAR)]<-0000
